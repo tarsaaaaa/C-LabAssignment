bfbc77cb81eea015599ae94692fd939cde29287e
tarsaaaaa <tarsaaaaa1624@gmail.com>
2026-01-08 13:41:02 +0545

    Update Qn7_.cpp

--- SakshyamShrestha/Lab-Sheet-2/Sources/Qn7_.cpp
+++ SakshyamShrestha/Lab-Sheet-2/Sources/Qn7_.cpp
@@ -7,25 +7,5 @@
 #include <stdio.h>
 
 int main() {
-    int n=0, count_pos=0, count_neg=0, count_zero=0;
-    char in;
-    printf("Start inputing numbers and enter 'q' to end\n");
-    while (true) {
-        scanf("%c", &in);
-        if (in == 'q') {
-            break;
-        }
-        
-        n = in - '0';
-        printf("\n%d\t%c", n, in);
-        if(n>0) {
-            count_pos++;
-        } else if(n<0) {
-            count_neg++;
-        } else {
-            count_zero++;
-        }
-    }
-    printf("Positive numbers entered=%d\nNegative numbers entered=%d\nZeros entered=%d", count_pos, count_neg, count_zero);
     return 0;
 }
