a86b5e2e8e36b67f430a92382f00ca004aacff9d
tarsaaaaa <tarsaaaaa1624@gmail.com>
2026-04-13 15:56:55 +0545

    Solved the flaw with max of negative numbers

--- SakshyamShrestha/Lab-Sheet-3/tempCodeRunnerFile.cpp
+++ SakshyamShrestha/Lab-Sheet-3/tempCodeRunnerFile.cpp
@@ -1,13 +1,4 @@
-/*
-    Write a program that asks the user to enter 10 numbers, reads them
-    into an array and finds sum, product, average and maximum of all
-    numbers and prints them
-*/
-
-#include <stdio.h>
-
-int main() {
-    int arr[10], sum = 0, prod = 1, max=0;
+int arr[10], sum = 0, prod = 1, max=0;
     float avg;
     printf("Enter 10 numbers\n");
     for (int  i = 0; i < 10; i++) {
@@ -22,4 +13,3 @@ int main() {
     avg = sum / 10;
     printf("Sum=%d\nProduct=%d\nAverage=%.2f\nMax=%d", sum, prod, avg, max);
     return 0;
-}
