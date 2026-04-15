d5a463bed1178bbac202024fb78ca782bc997ef3
tarsaaaaa <tarsaaaaa1624@gmail.com>
2026-04-15 19:34:30 +0545

    -

--- SakshyamShrestha/Lab-Sheet-6/Qn7.cpp
+++ SakshyamShrestha/Lab-Sheet-6/Qn7.cpp
@@ -16,7 +16,6 @@ int main() {
         return 1;
     }
 
-    // Input elements into the array
     printf("Enter %d numbers:\n", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
@@ -25,7 +24,6 @@ int main() {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j + 1]
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
